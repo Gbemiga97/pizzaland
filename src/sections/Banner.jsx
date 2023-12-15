c9197137a7cecc45494429f67de5d9b5d6cb2cc8
Gbemiga97 <holuwagbemiga97@gmail.com>
2023-12-15 13:39:39 +0100

    third commit

--- src/sections/Banner.jsx
+++ src/sections/Banner.jsx
@@ -1,7 +1,27 @@
+import { images } from "../utils"
+
 
 const Banner = () => {
   return (
-    <div>Banner</div>
+    <section className="bg-primary bg-pattern lg:min-[768px] pt-16 lg:pt-16">
+      <div className="container mx-auto min-h-[768px] flex items-center justify-center">
+          {/* text */}
+            <div className="flex flex-col xl:flex-row">
+              <div className="flex-1">
+                <h3 className="text-[2rem] uppercase tracking-[0.03rem]">
+                  Best pizza in town</h3>
+                <h1 className="text-6xl lg:text-8xl drop-shadow-md">
+                  Pizza perfection <br /> in every bite
+                </h1>
+              </div>
+
+          {/* images */}
+          <div>
+            <img src={images.PizzaBanner} alt="pizza" />
+          </div>
+            </div>
+      </div>
+    </section>
   )
 }
 
---
+++ src/utils/images.js
@@ -0,0 +1,68 @@
+import Badge from '../assets/badge.svg'
+import Bag from '../assets/bag.svg'
+import Capricciosa from '../assets/capricciosa.webp'
+import Chessy from '../assets/cheesy.webp'
+import Cherry from '../assets/cherry.png'
+import Chilli1 from '../assets/chilli-1.png'
+import Chilli2 from '../assets/chilli-2.png'
+import Corn from '../assets/corn.png'
+import Crown from '../assets/crown.svg'
+import FreshTomatoes from '../assets/fresh-tomatoes.png'
+import Garlic1 from '../assets/garlic-1.png'
+import Garlic2 from '../assets/garlic-2.png'
+import Garlic3 from '../assets/garlic-3.png'
+import Hawaii from '../assets/hawaii.webp'
+import Italian from '../assets/italian.webp'
+import Jalapeno from '../assets/jalapeno.png'
+import Leaves from '../assets/leaves.png'
+import Logo from '../assets/logo.svg'
+import Margherita from '../assets/margherita.webp'
+import Mozzarella from '../assets/mozzarella.png'
+import Mushrooms from '../assets/mushrooms.png'
+import Olives from '../assets/olives.png'
+import Parmesan from '../assets/parmesan.png'
+import  Pepperoni from '../assets/pepperoni.webp'
+import Phone from '../assets/phone.svg'
+import PizzaBanner from '../assets/pizza-banner.png'
+import QuattroFormaggi from '../assets/quattro-formaggi.webp'
+import QuattroStagioni from '../assets/quattro-stagioni.webp'
+import Success from '../assets/success-1.gif'
+import Tonno from '../assets/tonno.webp'
+import Vegetarian from '../assets/vegetarian.webp'
+
+
+const images = {
+    Badge,
+    Bag,
+    Capricciosa,
+    Chilli1,
+    Chilli2,
+    Cherry,
+    Corn,
+    Chessy,
+    Leaves,
+    Logo,
+    Margherita,
+    Crown,
+    FreshTomatoes,
+    Garlic1,
+    Garlic2,
+    Garlic3,
+    Hawaii,
+    Italian,
+    Jalapeno,
+    Mozzarella,
+    Mushrooms,
+    Olives,
+    Parmesan,
+    Pepperoni,
+    Phone,
+    PizzaBanner,
+    QuattroFormaggi,
+    QuattroStagioni,
+    Success,
+    Tonno,
+    Vegetarian
+}
+
+export default images
